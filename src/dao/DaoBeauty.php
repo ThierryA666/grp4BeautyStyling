8112e70ef80d8d32b9140a51d1a637cd74f23d22
Takako Onodera <takako.onodera@gmail.com>
2024-04-15 14:25:45 +0200

    methode delete salon by id  Changes to be committed: 	modified:   src/dao/DaoBeauty.php 	modified:   src/dao/Requetes.php

--- src/dao/DaoBeauty.php
+++ src/dao/DaoBeauty.php
@@ -126,8 +126,8 @@ class DaoBeauty {
             $query  = $this->conn->prepare($query);
             $query->execute([':id_salon' => $id_salon]);
             $row = $query->fetch(\PDO::FETCH_OBJ);
-            $tel_salon = strval($row->tel_salon); // 不要な場合は削除できます
-            $cp_salon = strval($row->cp_salon);   // 不要な場合は削除できます
+            $tel_salon = strval($row->tel_salon); 
+            $cp_salon = strval($row->cp_salon);   
             $salon = new Salon($row->id_salon, $row->nom_res, $row->prenom_res, $row->ad_1, $row->ad_2, $row->nom_salon, $row->email_salon, $cp_salon, $tel_salon, $row->url_salon, $row->photo_salon, $row->pw_salon, new \DateTime(), $row->nom_ville); 
         } catch (\Exception $e) {
             throw new \Exception('Exception !!! : ' .  $e->getMessage(), $this->convertCode($e->getCode()));
@@ -137,37 +137,6 @@ class DaoBeauty {
         return $salon;
     }
     
-
-    // public function updateSalonByID(Salon $salon){
-    //     $query = Requetes::UPDATE_SALON_BY_ID;
-    //     try{
-    //         $statement = $this->conn->prepare($query);
-    //         $statement->bindValue(':id_salon', $salon->getId_salon());
-    //         $statement->bindValue(':nom_res', $salon->getNom_res());
-    //         $statement->bindValue(':prenom_res', $salon->getPrenom_res());
-    //         $statement->bindValue(':ad_1', $salon->getAd1());
-    //         $statement->bindValue(':ad_2', $salon->getAd2());
-    //         $statement->bindValue(':email_salon', $salon->getEmail_salon());
-    //         //codepostal:string
-    //         $statement->bindValue(':cp_salon', (string)$salon->getCp_salon());
-    //         //concatener '0' pour numero de tel
-    //         $statement->bindValue(':tel_salon', '0' . $salon->getTel_salon());
-    //         $statement->bindValue(':url_salon',$salon->getUrl_salon());
-    //         $statement->bindValue(':photo_salon',$salon->getPhoto_salon());
-    //         $statement->bindValue(':pw_salon',$salon->getPw_salon());
-    //         $statement->bindValue(':nom_salon', $salon->getNom_salon());
-    //         $statement->bindValue(':nom_ville',$salon->getNom_ville());
-    //         $statement->execute();
-    
-    //     }
-    //     catch (\Exception $e) {
-    //         throw new \Exception('Exception  !!! : ' .  $e->getMessage() , $this->convertCode($e->getCode()));
-    //     }
-    //     catch (\Error $error) {
-    //         throw new \Exception('Error !!! : ' .  $error->getMessage());
-    //     }
-    // }
-
     public function updateSalonByID(Salon $salon){
         $query = Requetes::UPDATE_SALON_BY_ID;
         try{
@@ -199,10 +168,23 @@ class DaoBeauty {
     }
 
 
-    // public function delSalon(Salon $salon){
-    //     $query = Requetes::DELETE_SALON;
-    //     $statement = $this->conn->prepare($query);
-    // }
+    public function delSalonByID(Salon $salon){
+        $query = Requetes::DELETE_SALON_BY_ID;
+        try {
+            $query  = $this->conn->prepare($query);
+            $query->bindValue(':id_salon', $salon->getId_salon(), \PDO::PARAM_INT);
+            $query->execute();
+        }
+        catch (\PDOException $pdoe) {
+            throw $pdoe;
+        } 
+        catch (\Exception $e) {
+            throw $e;
+        }
+        catch (\Error $error) {
+            throw $error;
+        } 
+    }
     
 
 
--- src/dao/Requetes.php
+++ src/dao/Requetes.php
@@ -9,9 +9,47 @@ class Requetes{
 
     public const INSERT_SALON = "insert into `salon` values (:id_salon, :nom_res, :prenom_res, :ad_1, :ad_2, :nom_salon, :email_salon, :cp_salon, :tel_salon, :url_salon, :photo_salon, :pw_salon, :date_cre, :nom_ville )";
 
-    public const DELETE_SALON ="delete from `salon` where tel_salon = :tel_salon";
+    public const DELETE_SALON_BY_ID ="
+    SET @employe_fk_exist = (SELECT COUNT(*) FROM information_schema.KEY_COLUMN_USAGE WHERE CONSTRAINT_SCHEMA = 'beautystyling' AND TABLE_NAME = 'employe' AND CONSTRAINT_NAME = 'employe_ibfk_1');
+    SET @offrir_fk_exist = (SELECT COUNT(*) FROM information_schema.KEY_COLUMN_USAGE WHERE CONSTRAINT_SCHEMA = 'beautystyling' AND TABLE_NAME = 'offrir' AND CONSTRAINT_NAME = 'offrir_ibfk_2');
+    SET @reservation_fk_exist = (SELECT COUNT(*) FROM information_schema.KEY_COLUMN_USAGE WHERE CONSTRAINT_SCHEMA = 'beautystyling' AND TABLE_NAME = 'reservation' AND CONSTRAINT_NAME = 'reservation_ibfk_3');
+
+    IF @employe_fk_exist > 0 THEN
+        ALTER TABLE employe DROP FOREIGN KEY employe_ibfk_1;
+    END IF;
+
+    IF @offrir_fk_exist > 0 THEN
+        ALTER TABLE offrir DROP FOREIGN KEY offrir_ibfk_2;
+    END IF;
+
+    IF @reservation_fk_exist > 0 THEN
+        ALTER TABLE reservation DROP FOREIGN KEY reservation_ibfk_3;
+    END IF;
+
+    DELETE FROM salon WHERE id_salon = :id_salon;
+
+    IF @employe_fk_exist > 0 THEN
+        ALTER TABLE employe ADD CONSTRAINT employe_ibfk_1 FOREIGN KEY (id_salon) REFERENCES salon (id_salon);
+    END IF;
+
+    IF @offrir_fk_exist > 0 THEN
+        ALTER TABLE offrir ADD CONSTRAINT offrir_ibfk_2 FOREIGN KEY (id_salon) REFERENCES salon (id_salon);
+    END IF;
+
+    IF @reservation_fk_exist > 0 THEN
+        ALTER TABLE reservation ADD CONSTRAINT reservation_ibfk_3 FOREIGN KEY (id_salon) REFERENCES salon (id_salon);
+    END IF;
+";
 
     public const SELECT_SALON_BY_ID = "select id_salon, nom_res, prenom_res,  ad_1, ad_2, nom_salon, email_salon, cp_salon, tel_salon, url_salon, photo_salon, pw_salon, date_cre, nom_ville from salon WHERE id_salon = :id_salon";
 
     public const UPDATE_SALON_BY_ID = "update salon SET nom_res =:nom_res, prenom_res=:prenom_res,  ad_1=:ad_1, ad_2=:ad_2, nom_salon=:nom_salon,email_salon=:email_salon, cp_salon=:cp_salon, tel_salon=:tel_salon, url_salon=:url_salon, photo_salon=:photo_salon, pw_salon=:pw_salon, nom_ville=:nom_ville  WHERE id_salon = :id_salon";
 }
+
+// alter table employe drop foreign key employe_ibfk_1;
+// alter table offrir drop foreign key offrir_ibfk_2;
+// alter table reservation drop foreign key reservation_ibfk_3;
+// delete from salon where id_salon =3;
+// alter table employe add constraint employe_ibfk_1 foreign key(id_salon) references salon(id_salon);
+// alter table offrir add constraint offrir_ibfk_2 foreign key(id_salon) references salon(id_salon);
+// alter table reservation add constraint reservation_ibfk_3 foreign key(id_salon) references salon(id_salon);
