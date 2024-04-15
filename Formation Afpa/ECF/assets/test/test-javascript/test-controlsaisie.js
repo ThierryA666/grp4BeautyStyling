6604f4414439b96bb9b7cc728878f39cc5136ece
FocaMonje <123114660+FocaMonje@users.noreply.github.com>
2024-04-15 11:04:14 +0200

    Add files via upload

---
+++ Formation Afpa/ECF/assets/test/test-javascript/test-controlsaisie.js
@@ -0,0 +1,32 @@
+
+
+function controlSaisie(date, detail){
+
+    if (date===Number(date)) {
+        return "Une erreur s'est produite, tapez une date (Ex: 15 Février 2024)";
+    } 
+    else if (date===""){
+       return "Une erreur s'est produite, tapez une date (Ex: 15 Février 2024)";
+    } 
+    else if (detail===Number(detail)){
+       return "Une erreur s'est produite, tapez votre message";
+    } 
+    else {
+        return "donnees()";
+    }
+}
+    
+    
+const {test} = QUnit;
+    test('T1a - cas nominal', function(assert) {
+        assert.equal(controlSaisie(10, 'aucun'), "Une erreur s'est produite, tapez une date (Ex: 15 Février 2024)");
+    });
+    test('T2 - cas nominal', function(assert) {
+        assert.equal(controlSaisie("", 'aucun'), "Une erreur s'est produite, tapez une date (Ex: 15 Février 2024)");
+    });
+    test('T3 - cas nominal', function(assert) {
+        assert.equal(controlSaisie('15 Février', 5), "Une erreur s'est produite, tapez votre message");
+    });
+    test('T4 - cas nominal', function(assert) {
+        assert.equal(controlSaisie('15 Février', 'aucun'), "donnees()");
+});
