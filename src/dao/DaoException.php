53933bcc4011be5162354acead8f7355d402b6f8
Maria <mari11carmen_1989@hotmail.com>
2024-04-16 12:00:37 +0200

    essai commit de vscode à git hub

--- src/dao/DaoException.php
+++ src/dao/DaoException.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 declare(strict_types=1);
 namespace beautyStyling\dao;
@@ -7,14 +6,4 @@ class DaoException extends \Exception {
     
 }
 
-=======
-<?php
-declare(strict_types=1);
-namespace beautyStyling\dao;
-
-class DaoException extends \Exception {
-    
-}
-
->>>>>>> a6980d88292d460c429dea4d66edcf90a6e13bb2
 ?>
--- src/metier/Etat.php
+++ src/metier/Etat.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -36,43 +35,4 @@ class Etat {
     }
 }
 
-=======
-<?php
-
-declare(strict_types=1);
-namespace beautyStyling\metier;
-
-use beautyStyling\dao\Database;
-use beautyStyling\dao\Requetes;
-use beautyStyling\dao\DaoCalendrier;
-use beautyStyling\metier\Reservation;
-
-
-class Etat {
-    private int     $id_etat;
-    private String  $libel_etat;
-
-    public function __construct($id_etat, $libel_etat) {
-        $this->id_etat       = $id_etat;
-        $this->libelle  = $libel_etat;
-    }
-
-    public function getId_etat(): int {
-        return $this->id_etat;
-    }
-    public function setId(int $id_etat) {
-        $this->id_etat = $id_etat;
-    }
-    public function getLibel_etat(): String {
-        return $this->libel_etat;
-    }
-    public function setLibel_etat(String $libel_etat) {
-        $this->libel_etat = $libel_etat;
-    }
-    public function __toString() {
-        return '[Etat : '.$this->id_etat . ',' . $this->libel_etat .']';
-    }
-}
-
->>>>>>> a6980d88292d460c429dea4d66edcf90a6e13bb2
 ?>
