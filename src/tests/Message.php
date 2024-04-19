fc3232a5688a2a4579a69d4f66367ee9f03203de
Thierry Anglade <thierryanglade66@gmail.com>
2024-04-19 20:50:15 +0200

    start from scratch 	modified:   src/view/vpopUpSalon.php     new file:   src/webapp/MyException.php 	modified:   src/webapp/popUpSalon.php

---
+++ src/tests/Message.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+namespace beautyStyling\tests;
+
+// enum Message {
+//     case CategorieInvalide;
+//     case PlatInvalide;
+
+//     public function getMessage(): string {
+//         return match($this) {
+//             self::CategorieInvalide => 'Cette categorie est inexistante',
+//             self::PlatInvalide      => 'Ce plat est inexistant',
+
+//             // Suit::Clubs, Suit::Spades => 'Black',
+//         };
+//     }
+
+//     public function getCode(): int {
+//         return match($this) {
+//             self::CategorieInvalide => 8001,
+//             self::PlatInvalide => 8002,
+//         };
+//     }
+    
+// }
