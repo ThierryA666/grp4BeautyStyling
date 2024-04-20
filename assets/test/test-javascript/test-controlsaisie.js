f8d808025ed3ee707a5420086c0689c78b2e84c4
Takako Onodera <takako.onodera@gmail.com>
2024-04-20 17:42:42 +0200

    takako merge avec system-test 	modified:   assets/css/style-calendrier.css 	modified:   assets/html/random.html 	modified:   assets/img/logo.svg 	modified:   assets/js/salon_profile.js 	modified:   assets/test/test-javascript/test-class.js 	modified:   assets/test/test-javascript/test-controlsaisie.js 	modified:   html/adminListePrestations.html 	modified:   html/adminPrestation.html 	modified:   html/calendrier.html 	modified:   html/clientDetailPanier.html 	modified:   html/clientPaniers.html 	modified:   html/creneau.html 	modified:   html/gestion-comptes-prestataires.html 	modified:   html/historiquedesrendezvous.html 	modified:   html/index.html 	modified:   html/rendezvouscoteclient.html 	modified:   html/salon_application.html 	modified:   html/salon_gestionnaire.html 	modified:   html/salon_login.html 	modified:   html/salon_profile.html 	modified:   html/salon_top.html 	modified:   html/testQUnit_takako.html 	modified:   param.ini

--- assets/test/test-javascript/test-controlsaisie.js
+++ assets/test/test-javascript/test-controlsaisie.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function controlSaisie(date, detail){
 
     if (date===Number(date)) {
@@ -27,4 +28,35 @@ const {test} = QUnit;
     });
     test('T4 - cas nominal', function(assert) {
         assert.equal(controlSaisie('15 Février', 'aucun'), "donnees()");
+=======
+function controlSaisie(date, detail){
+
+    if (date===Number(date)) {
+        return "Une erreur s'est produite, tapez une date (Ex: 15 Février 2024)";
+    } 
+    else if (date===""){
+       return "Une erreur s'est produite, tapez une date (Ex: 15 Février 2024)";
+    } 
+    else if (detail===Number(detail)){
+       return "Une erreur s'est produite, tapez votre message";
+    } 
+    else {
+        return "donnees()";
+    }
+}
+    
+    
+const {test} = QUnit;
+    test('T1a - cas nominal', function(assert) {
+        assert.equal(controlSaisie(10, 'aucun'), "Une erreur s'est produite, tapez une date (Ex: 15 Février 2024)");
+    });
+    test('T2 - cas nominal', function(assert) {
+        assert.equal(controlSaisie("", 'aucun'), "Une erreur s'est produite, tapez une date (Ex: 15 Février 2024)");
+    });
+    test('T3 - cas nominal', function(assert) {
+        assert.equal(controlSaisie('15 Février', 5), "Une erreur s'est produite, tapez votre message");
+    });
+    test('T4 - cas nominal', function(assert) {
+        assert.equal(controlSaisie('15 Février', 'aucun'), "donnees()");
+>>>>>>> b3737144e9af770f87c5acb1ac273df8b56038c9
 });
